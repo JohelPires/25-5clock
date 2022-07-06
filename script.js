98a424ba94016aa86c5ac766e8c942c32fc142a9
JohelPires <johelpires@gmail.com>
2022-07-06 10:45:18 -0400

    break and session increment decrement

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-const timer = setInterval(
-  () =>
-    setTheClock((prevTC) => ({
-      ...prevTC,
-      timeLeft: prevTC.timeLeft - 1,
-    })),
-  1000
-)
+const counter = 1500
+let breakLengthValue = 5
+let sessionLengthValue = 25
 
-function breakIncrement() {
-  alert('break increment')
+const breakLength = document.getElementById('break-length')
+const sessionLength = document.getElementById('session-length')
+const timeLeft = document.getElementById('time-left')
+
+// const timer = setInterval(
+//   () =>
+//     setTheClock((prevTC) => ({
+//       ...prevTC,
+//       timeLeft: prevTC.timeLeft - 1,
+//     })),
+//   1000
+// )
+
+function breakIncrement(event) {
+  switch (event.target.id) {
+    case 'break-increment':
+      breakLengthValue++
+      breakLength.innerText = breakLengthValue
+      break
+    case 'break-decrement':
+      breakLengthValue--
+      breakLength.innerText = breakLengthValue
+      break
+    case 'session-increment':
+      sessionLengthValue++
+      sessionLength.innerText = sessionLengthValue
+      break
+    case 'session-decrement':
+      sessionLengthValue--
+      sessionLength.innerText = sessionLengthValue
+      break
+
+    default:
+      break
+  }
+
+  console.log(event.target.id)
+}
+function toggleTimer() {
+  alert('toggle timer')
+}
+function handleReset() {
+  timeLeft.innerText = '25:00'
 }
