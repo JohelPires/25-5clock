c8b99121209762d311aee933b4dcd9ea899af00c
JohelPires <johelpires@gmail.com>
2022-07-06 10:59:13 -0400

    js update

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const counter = 1500
+let counter = 1500
 let breakLengthValue = 5
 let sessionLengthValue = 25
+let running = false
 
 const breakLength = document.getElementById('break-length')
 const sessionLength = document.getElementById('session-length')
 const timeLeft = document.getElementById('time-left')
+timeLeft.innerText = counter
 
 // const timer = setInterval(
 //   () =>
@@ -18,31 +20,37 @@ const timeLeft = document.getElementById('time-left')
 function breakIncrement(event) {
   switch (event.target.id) {
     case 'break-increment':
-      breakLengthValue++
+      breakLengthValue < 60 && breakLengthValue++
       breakLength.innerText = breakLengthValue
       break
     case 'break-decrement':
-      breakLengthValue--
+      breakLengthValue > 1 && breakLengthValue--
       breakLength.innerText = breakLengthValue
       break
     case 'session-increment':
-      sessionLengthValue++
+      !running && sessionLengthValue < 60 && sessionLengthValue++
       sessionLength.innerText = sessionLengthValue
+
       break
     case 'session-decrement':
-      sessionLengthValue--
+      !running && sessionLengthValue > 1 && sessionLengthValue--
       sessionLength.innerText = sessionLengthValue
       break
 
     default:
       break
   }
-
-  console.log(event.target.id)
+  counter = sessionLengthValue * 60
+  timeLeft.innerText = counter
 }
 function toggleTimer() {
   alert('toggle timer')
 }
 function handleReset() {
-  timeLeft.innerText = '25:00'
+  counter = 1500
+  sessionLengthValue = 25
+  sessionLength.innerText = sessionLengthValue
+  breakLengthValue = 5
+  breakLength.innerText = breakLengthValue
+  timeLeft.innerText = counter
 }
